7bb1bca664ca6e49a8058c5f1f1a11b841cde998
Woody <39872036+Tazhio@users.noreply.github.com>
2019-10-08 23:45:49 +0800

    #added some file

--- hw2/helloworldWebServer.py
+++ hw2/helloworldWebServer.py
@@ -3,7 +3,17 @@ import socket
 
 hello = [b'HTTP/1.0 200 OK\r\n',
 b'Connection: close' b'Content-Type:text/html; charset=utf-8\r\n', b'\r\n',
-    b'<html><body>Hello World!<body></html>\r\n',
+    b'''<html><head><title>Index of .//</title></head>
+    <body bgcolor="white">
+    <h1>Index of .//</h1><hr>
+    <pre>
+    <a href="dir1/">dir1/</a><br>
+    <a href="dir2/">dir2/</a><br>
+    <a href="file1">file1</a><br>
+    <a href="file1">file1</a><br>
+    </pre>
+    <hr>
+    </body></html>   ''' ,
     b'\r\n']
 
 
@@ -27,6 +37,25 @@ def web():
         for line in res:
             conn.send(line)
         conn.close()
+
+keys = ('method', 'path')
+
+class HTTPHeader:
+    def __init__(self):
+        self.headers = {key: None for key in keys}
+
+    def parse_header(self, line):
+        fileds = line.split(' ')
+        if fileds[0] == 'GET' or fileds[0] == 'HEAD':
+            self.headers['method'] = fileds[0]
+            self.headers['path'] = fileds[1]
+        else:return 405
+
+
+    def get(self, key):
+        return self.headers.get(key)
+
+
 if __name__=='__main__':
     try:
         web()
--- lab4/asyncioWebHello.py
+++ lab4/asyncioWebHello.py
@@ -1,6 +1,35 @@
 import asyncio
 
 
+keys = ('method', 'path')
+
+err404 = [b'HTTP/1.0 404 Not Found\r\n',
+    b'Connection: close'
+    b'Content-Type:text/html; charset=utf-8\r\n',
+    b'\r\n',
+b'<html><body>404 Not Found<body></html>\r\n', b'\r\n']
+err504 = [b'HTTP/1.0 404 Not Found\r\n',
+    b'Connection: close'
+    b'Content-Type:text/html; charset=utf-8\r\n',
+    b'\r\n',
+b'<html><body>504 Method Not Allowed<body></html>\r\n', b'\r\n']
+
+class HTTPHeader:
+    def __init__(self):
+        self.headers = {key: None for key in keys}
+
+    def parse_header(self, line):
+        fileds = line.split(' ')
+        if fileds[0] == 'GET' or fileds[0] == 'HEAD':
+            #to check whether the method is allowed or not
+            self.headers['method'] = fileds[0]
+            self.headers['path'] = fileds[1]
+        else:return 504
+
+    def get(self, key):
+        return self.headers.get(key)
+
+
 async def dispatch(reader, writer):
     while True:
         data = await reader.readline()
@@ -13,7 +42,18 @@ async def dispatch(reader, writer):
         b'Content-Type:text/html; charset=utf-8\r\n',
         b'Connection: close\r\n',
         b'\r\n',
-        b'<html><body>Hello World!<body></html>\r\n',
+        b''' <html><head><title>Index of .//</title></head>
+    <body bgcolor="white">
+    <h1>Index of .//</h1><hr>
+    <pre>
+    <a href="dir1/">dir1/</a><br>
+    <a href="dir2/">dir2/</a><br>
+    <a href="file1">file1</a><br>
+    <a href="file1">file1</a><br>
+    </pre>
+    <hr>
+    </body></html>
+\r\n''',
         b'\r\n'
     ])
     await writer.drain()
@@ -32,3 +72,4 @@ if __name__ == '__main__':
 server.close()
 loop.run_until_complete(server.wait_closed())
 loop.close()
+
